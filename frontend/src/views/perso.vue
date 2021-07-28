30941de65679c9203224f3ba118c21fd3d7bd425
Animasso <60935027+Animasso@users.noreply.github.com>
2021-07-28 21:01:08 +0200

    ajout

--- frontend/src/views/perso.vue
+++ frontend/src/views/perso.vue
@@ -11,7 +11,7 @@
                                 <label for="exampleFormControlTextarea5">Postez votre message</label>
                                 <textarea class="form-control" id="exampleFormControlTextarea5" rows="3"></textarea>
                             </div>
-                            <button class="btn btn-primary pull-right" type="submit" @click="postMessage">Partager</button>
+                            <button class="btn btn-primary pull-right"  @click="postMessage()">Partager</button>
                         </div>
                     </div>
                 </div>
@@ -20,15 +20,15 @@
 
         
         <div class="card row-8 p-3 border-blue mt-3">  
-                    <div class="post">
+                    <div v-for="posts in posts" v-bind:key="posts.id" class="post">
                         <div class="d-flex justify-content-between mt-2">
                     <div class="d-flex flex-row">
                         <div class="d-flex flex-column">
-                            <h6 class="mb-0">Animasso Sidibé</h6> <span class="date">Nov 30, 2020 at 10:40</span>
+                            <h6 class="mb-0"> {{posts.user.firstName}}{{posts.user.lastName}} </h6> <span class="date">Nov 30, 2020 at 10:40</span>
                         </div>
                     </div>
                     </div>
-                        <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies, odio in interdum tristique, erat arcu vestibulum leo, non condimentum eros risus quis tellus.</p>
+                        <p class="content">{{content}} </p>
                         <div class="mt-2 d-flex justify-content-end"> <button class="btn btn-primary btn-sm ms-1" type="submit" @click.prevent="deletePost(post)">Supprimer</button> </div>
                     </div>
             
@@ -81,28 +81,31 @@ export default {
          .catch((err) => console.log(err));
 
 
-         axios.get("http://localhost:3000/api/posts", {
+         axios.get("http://localhost:3000/api/posts/:id/"+comments, {
             headers: {
                Authorization: "Bearer " + sessionStorage.token,
             },
          })
-         .then(response => 
-          (this.posts = response.data.posts))
+         .then((response)=> 
+         console.log(response))
+          
          .catch(err => console.log(err))
     },
 
 methods :{
 postMessage(){
+    const userId= sessionStorage.getItem('user')
         axios
-        .post("http://localhost:3000/api/posts/post")
+        .post("http://localhost:3000/api/posts/post/" + userId)
         .then(response=>{
             this.posts=response.data;
             this.$router.push("perso")
-        })
+        }) 
     },
     deletePost() {
+        const userId= sessionStorage.getItem('user')
          axios
-            .delete("http://localhost:3000/api/posts/:id" , {
+            .delete("http://localhost:3000/api/posts/"+ userId, {
                headers: {
                   Authorization: "Bearer " + sessionStorage.token,
                },
